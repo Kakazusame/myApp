c0993af2e7aab83b04bdedc9826823b50bbdf9b1
嘉数涼夏 <same777mydog@gmail.com>
2018-03-02 11:07:21 +0800

    ボタンのデザインチェンジ

--- myApp/myApp/questionViewController.swift
+++ myApp/myApp/questionViewController.swift
@@ -449,8 +449,6 @@ class questionViewController: UIViewController, UINavigationControllerDelegate, 
     }
     /////ここまでCoreData///////
     
-    
-    
 }
     /*
     // MARK: - Navigation
@@ -474,5 +472,30 @@ extension Array where Element: Equatable {
     }
     
 }
+
+/////ここからボタン///////
+
+@IBDesignable class CustomButton: UIButton {
+    
+    // 角丸の半径(0で四角形)
+    @IBInspectable var cornerRadius: CGFloat = 0.0
+    
+    // 枠
+    @IBInspectable var borderColor: UIColor = UIColor.clear
+    @IBInspectable var borderWidth: CGFloat = 0.0
+    
+    override func draw(_ rect: CGRect) {
+        // 角丸
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = (cornerRadius > 0)
+        
+        // 枠線
+        self.layer.borderColor = borderColor.cgColor
+        self.layer.borderWidth = borderWidth
+        
+        super.draw(rect)
+    }
+}
+/////ここまでボタン///////
 
 
